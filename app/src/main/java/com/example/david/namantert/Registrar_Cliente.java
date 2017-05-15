e8082e3e443fa10fe275b741941ec5d92ba0131e
David Rodriguez <davidrch2013@gmail.com>
2017-05-15 15:53:30 -0500

    Interfaz terminada

---
+++ app/src/main/java/com/example/david/namantert/Registrar_Cliente.java
@@ -0,0 +1,13 @@
+package com.example.david.namantert;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Registrar_Cliente extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar__cliente);
+    }
+}
